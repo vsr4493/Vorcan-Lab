8ef821d501bf5c1f2424305423d36c0866b3dd5a
vardhman@4waytechnologies.com <vardhman@4waytechnologies.com>
2017-08-14 19:15:42 +0530

    Heroku build attempt

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000
+const app = next({ dir: '.', dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = express()
+
+  server.get('/about', (req, res) => {
+    return app.render(req, res, '/about', req.query)
+  })
+
+  server.get('/index', (req, res) => {
+    return app.render(req, res, '/index', req.query)
+  })
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
